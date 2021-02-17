af396728a9a817722c0daa18b3b72cdef147524c
lucasstarsz <andrewrcdey@gmail.com>
2021-02-16 22:16:59 -0500

    Remove unnessecary random number initialization.

--- src/examples/java/examples/guessinggame/components/NumberHolder.java
+++ src/examples/java/examples/guessinggame/components/NumberHolder.java
@@ -3,5 +3,5 @@ package examples.guessinggame.components;
 import io.github.lucasstarsz.slopeecs.component.IComponent;
 
 public class NumberHolder implements IComponent {
-    public int number = (int) ((Math.random() * 10) + 1);
+    public int number;
 }
