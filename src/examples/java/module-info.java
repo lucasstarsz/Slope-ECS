97bc15828de1b2aa918da84ee015b136eedd0620
lyntier <64606744+Lyntier@users.noreply.github.com>
2021-02-25 14:26:25 +0100

    Replace package exposure with limited export to slope.ecs module

--- src/examples/java/module-info.java
+++ src/examples/java/module-info.java
@@ -1,6 +1,7 @@
 /**
  * This module contains all the examples for <a href="https://github.com/lucasstarsz/Slope-ECS">Slope ECS</a>.
  */
-open module Slope.ECS.examples {
+module Slope.ECS.examples {
     requires slope.ecs;
+    opens examples.guessinggame.systems to slope.ecs;
 }
