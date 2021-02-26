4add5c5da6876f73b57fc33871833b3bca05cecb
lucasstarsz <andrewrcdey@gmail.com>
2021-02-26 09:20:37 -0500

    Add sufficient documentation (#12)

--- src/main/java/io/github/lucasstarsz/slopeecs/entity/ECSEntityManager.java
+++ src/main/java/io/github/lucasstarsz/slopeecs/entity/ECSEntityManager.java
@@ -1,10 +1,26 @@
 package io.github.lucasstarsz.slopeecs.entity;
 
+import io.github.lucasstarsz.slopeecs.World;
+import io.github.lucasstarsz.slopeecs.component.ECSComponentManager;
+import io.github.lucasstarsz.slopeecs.system.ECSSystemManager;
+
 import java.util.BitSet;
 import java.util.Deque;
 import java.util.LinkedList;
 
-/** The manager of entities, entity counts, and entity signatures. */
+/**
+ * The manager of entities, entity counts, and entity signatures.
+ * <p>
+ * <h3>About</h3>
+ * This class is one of three managers (see: {@link ECSSystemManager}, {@link ECSComponentManager}) used within
+ * Slope-ECS. It serves the main purpose of storing entities and their signatures, as well as creation/destruction of
+ * those entities and their signatures.
+ * <p>
+ * Considering this is only one of three managers in Slope-ECS, it is better to use the {@link World} class to manage
+ * the ECS. In order to see that class in action, you should check the
+ * <a href="https://github.com/lucasstarsz/Slope-ECS/wiki" target="_blank">wiki</a> -- it is the best way to get an
+ * understanding of how to make use of Slope.
+ */
 public class ECSEntityManager {
 
     /** The array of signatures for each entity. */
@@ -24,7 +40,22 @@ public class ECSEntityManager {
     private final int maxEntities;
 
     /**
-     * Initializes the entity manager queue with all possible entity IDs.
+     * Constructs the entity manager queue with all possible entity IDs.
+     * <p>
+     * <h3>About</h3>
+     * This constructor requires that you set the maximum entity count, foregoing use of a default value. The value
+     * specified must be at least 1.
+     * <p>
+     * <h3>Example Usages</h3>
+     * <pre>{@code
+     * ECSEntityManager entityManager = new ECSEntityManager(100);
+     * System.out.println("Maximum entities allowed: " + entityManager.getAvailableEntities());
+     * System.out.println("Entities currently created: " + entityManager.getLivingEntityCount());
+     *
+     * // This code will print the following:
+     * // Maximum entities allowed: 100
+     * // Entities currently created: 0
+     * }</pre>
      *
      * @param maxEntityCount The maximum amount of entities to allow in the ECS.
      */
@@ -39,6 +70,28 @@ public class ECSEntityManager {
 
     /**
      * Creates an entity in the next available slot, then returns the entity's ID.
+     * <p>
+     * <h3>About</h3>
+     * This is the method called by {@link World#createEntity()}, allowing you to create an entity within Slope-ECS. For
+     * more information, see {@link World#createEntity()}.
+     * <p>
+     * <h3>Example Usages</h3>
+     * <pre>{@code
+     * ECSEntityManager entityManager = new ECSEntityManager(100);
+     * System.out.println("Available entity slots, before creation: " + entityManager.getAvailableEntities());
+     * System.out.println("Entities currently created, before creation: " + entityManager.getLivingEntityCount());
+     *
+     * int entity = entityManager.createEntity();
+     *
+     * System.out.println("Available entity slots, after creation: " + entityManager.getAvailableEntities());
+     * System.out.println("Entities currently created, after creation: " + entityManager.getLivingEntityCount());
+     *
+     * // This code will print the following:
+     * // Available entity slots, before creation: 100
+     * // Entities currently created, before creation: 0
+     * // Available entity slots, after creation: 99
+     * // Entities currently created, after creation: 1
+     * }</pre>
      *
      * @return The ID of the created entity.
      */
@@ -55,7 +108,48 @@ public class ECSEntityManager {
     }
 
     /**
-     * Destroys the entity of the specified type, invalidating its signature and moving it to the back of the queue.
+     * Destroys the entity of the specified type, invalidating its signature and moving it to the back of the {@link
+     * #availableEntities} queue.
+     * <p>
+     * <h3>About</h3>
+     * This is the method called by {@link World#createEntity()}, allowing you to create an entity within Slope-ECS. For
+     * more information, see {@link World#createEntity()}.
+     * <p>
+     * <h3>Example Usages</h3>
+     * <pre>{@code
+     * ECSEntityManager entityManager = new ECSEntityManager(100);
+     * int entity1 = entityManager.createEntity();
+     * int entity2 = entityManager.createEntity();
+     * int entity3 = entityManager.createEntity();
+     *
+     * System.out.println("Available entity slots, before destruction: " + entityManager.getAvailableEntities());
+     * System.out.println("Entities currently created, before destruction: " + entityManager.getLivingEntityCount());
+     *
+     * entityManager.destroyEntity(entity1);
+     *
+     * System.out.println("Available entity slots, after destruction 1: " + entityManager.getAvailableEntities());
+     * System.out.println("Entities currently created, after destruction 1: " + entityManager.getLivingEntityCount());
+     *
+     * entityManager.destroyEntity(entity2);
+     *
+     * System.out.println("Available entity slots, after destruction 2: " + entityManager.getAvailableEntities());
+     * System.out.println("Entities currently created, after destruction 2: " + entityManager.getLivingEntityCount());
+     *
+     * entityManager.destroyEntity(entity3);
+     *
+     * System.out.println("Available entity slots, after destruction 3: " + entityManager.getAvailableEntities());
+     * System.out.println("Entities currently created, after destruction 3: " + entityManager.getLivingEntityCount());
+     *
+     * // This code will print the following:
+     * // Available entity slots, before destruction: 97
+     * // Entities currently created, before destruction: 3
+     * // Available entity slots, after destruction 1: 98
+     * // Entities currently created, after destruction 1: 2
+     * // Available entity slots, after destruction 2: 99
+     * // Entities currently created, after destruction 2: 1
+     * // Available entity slots, after destruction 3: 100
+     * // Entities currently created, after destruction 3: 0
+     * }</pre>
      *
      * @param entity The ID of the entity to destroy.
      */
@@ -74,12 +168,6 @@ public class ECSEntityManager {
         livingEntityCount--;
     }
 
-    /**
-     * Sets the signature of the specified entity to the specified signature.
-     *
-     * @param entity    The entity to set the signature of.
-     * @param signature The signature to set.
-     */
     public void setSignature(int entity, BitSet signature) {
         if (!isAlive(entity)) {
             throw new IllegalStateException("Entity " + entity + " is not alive in the ECS.");
@@ -89,12 +177,6 @@ public class ECSEntityManager {
         entitySignatures[entity] = signature;
     }
 
-    /**
-     * Gets the signature of the specified entity.
-     *
-     * @param entity The entity to get the signature for.
-     * @return The signature corresponding to the specified entity.
-     */
     public BitSet getSignature(int entity) {
         if (!isAlive(entity)) {
             throw new IllegalStateException("Entity " + entity + " is not alive in the ECS.");
@@ -104,12 +186,6 @@ public class ECSEntityManager {
         return entitySignatures[entity];
     }
 
-    /**
-     * Checks whether the specified entity is currently alive.
-     *
-     * @param entity The entity to check.
-     * @return Whether the entity exists.
-     */
     public boolean isAlive(int entity) {
         if (entity < 0 || entity > maxEntities) {
             throw new IllegalStateException("Entity " + entity + " out of range.");
@@ -124,20 +200,10 @@ public class ECSEntityManager {
         return true;
     }
 
-    /**
-     * Gets the count of entities currently alive.
-     *
-     * @return The count of entities currently alive.
-     */
     public int getLivingEntityCount() {
         return livingEntityCount;
     }
 
-    /**
-     * Gets the number of remaining entity slots.
-     *
-     * @return The number of remaining entity slots.
-     */
     public int getAvailableEntities() {
         return availableEntities.size();
     }
