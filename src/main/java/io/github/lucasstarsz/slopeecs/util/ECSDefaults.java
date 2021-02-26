a3bb9f3d731c7f3c81baf2d64b5a867f48144239
lucasstarsz <andrewrcdey@gmail.com>
2021-02-26 09:06:28 -0500

    Add documentation to meet standards (#12)

--- src/main/java/io/github/lucasstarsz/slopeecs/util/ECSDefaults.java
+++ src/main/java/io/github/lucasstarsz/slopeecs/util/ECSDefaults.java
@@ -1,6 +1,14 @@
 package io.github.lucasstarsz.slopeecs.util;
 
-/** Default values for the ECS. */
+import io.github.lucasstarsz.slopeecs.World;
+
+/**
+ * Default values for the ECS.
+ * <p>
+ * <h3>About</h3>
+ * This class provides default values used across the entire ECS -- specifically when the {@link World} class is
+ * initialized without specifying the maximum entity amount (see: {@link World#init()}.
+ */
 public class ECSDefaults {
     /** The default maximum number of entities allowed. */
     public static final int defaultMaxEntityCount = 1000;
