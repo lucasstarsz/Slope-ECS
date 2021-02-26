2549f5f946d773b9f915ceafec7f73d85465224a
lucasstarsz <andrewrcdey@gmail.com>
2021-02-26 09:54:05 -0500

    Add formatted documentation (#12)

--- src/main/java/io/github/lucasstarsz/slopeecs/component/IComponent.java
+++ src/main/java/io/github/lucasstarsz/slopeecs/component/IComponent.java
@@ -3,10 +3,12 @@ package io.github.lucasstarsz.slopeecs.component;
 /**
  * This interface groups all components under one branch, allowing for generics to work as intended.
  * <p>
- * As a side note, all classes implementing the {@code IComponent} interface should follow these conventions:
+ * <h3>About</h3>
+ * All classes implementing the {@code IComponent} interface should follow these conventions:
  * <ul>
  *     <li>Like C++ structs, members should <strong>always</strong> be public.</li>
- *     <li>These components are designed as Plain Old Data (POD). As such they should not contain logic, only data to be operated on.</li>
+ *     <li>These components are designed as Plain Old Data (POD). As such they should not contain logic, only data to be
+ *     operated on.</li>
  * </ul>
  */
 public interface IComponent {
