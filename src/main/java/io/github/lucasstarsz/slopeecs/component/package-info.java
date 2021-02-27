bbb12f24db84a8770076436210306e30eb068637
lucasstarsz <andrewrcdey@gmail.com>
2021-02-27 11:58:19 -0500

    (#12) add <p> tag between paragraphs

--- src/main/java/io/github/lucasstarsz/slopeecs/component/package-info.java
+++ src/main/java/io/github/lucasstarsz/slopeecs/component/package-info.java
@@ -1,9 +1,7 @@
 /**
  * The package whose content all relates to ECS components.
- *
+ * <p>
  * For more information on how components work, check out the
- * <a href="https://github.com/lucasstarsz/Slope-ECS/wiki/Components" target="_blank"> entities wiki</a> -- it gives
- * the
- * best idea on how to use them.
+ * <a href="https://github.com/lucasstarsz/Slope-ECS/wiki/Components" target="_blank">components wiki</a>.
  */
 package io.github.lucasstarsz.slopeecs.component;
